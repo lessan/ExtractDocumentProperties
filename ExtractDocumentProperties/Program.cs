05439d799452db0ca26d8a6e03dff10be0c3bd34
Lessan Vaezi <lessan@gmail.com>
2011-06-29 15:12:08 +0300

    Initial commit

---
+++ ExtractDocumentProperties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml.CustomProperties;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace ExtractDocumentProperties
+{
+    class Program
+    {
+        public static Dictionary<string, string> GetCustomPropertiesOfWordDocument(string filename)
+        {
+            using (var package = WordprocessingDocument.Open(filename, false))
+            {
+                var properties = new Dictionary<string, string>();
+                foreach (var property in package.CustomFilePropertiesPart.Properties.Elements<CustomDocumentProperty>())
+                {
+                    var value = property.VTLPWSTR == null ? "" : property.VTLPWSTR.Text;
+                    properties.Add(property.Name, value);
+                }
+                return properties;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Invalid arguments! Usage: ExtractDocumentProperties filename.docx");
+                return;
+            }
+
+            var filename = args[0];
+            foreach (var prop in GetCustomPropertiesOfWordDocument(filename))
+            {
+                Console.WriteLine("{0}:{1}:{2}", filename, prop.Key, prop.Value);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+This is a sample project for working with the [Open XML SDK 2.0 for Microsoft Office](http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=5124).
+
+It is a console project using Visual Studio 2010 that accepts a filename and extracts all Custom Properties from the Word document.
+
+Syntax
+------
+    ExtractDocumentProperties filename.docx
